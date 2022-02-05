391b91b1855366f374b7c704d94cb0c0a04cf2e7
anne <88703094+viviann3@users.noreply.github.com>
2022-02-05 13:52:11 +0000

    finally added restrictions to lock & unlock

--- commands/moderation/guild/lock.js
+++ commands/moderation/guild/lock.js
@@ -8,9 +8,9 @@ module.exports = {
   run: async (client, message, args) => {
     const channel = message.channel || message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.guild.channels.cache.find((u) => u.name === args[0])
 
-    if (!channel) return message.reply({ content: 'I could not find that channel.'})
-
-    let msg = await message.channel.send({ content: 'Locking...'})
+    if (!channel) return message.reply({ content: 'I could not find that channel.'}) 
+    if (!message.member.permissions.has('MANAGE_CHANNELS')) return message.reply('Lock failed: Command was runned by a user that did not have the correct permissions to lock channels.')
+     let msg = await message.channel.send({ content: 'Locking...'})
 
 
       channel.permissionOverwrites.edit(message.guild.roles.cache.find( (e) => e.name.toLowerCase().trim() === "@everyone" ),
